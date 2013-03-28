6a51394e9e7bae6a67dfe1fe75645cd7705838a8
kyama <yamasaki.k.1101@gmail.com>
2013-03-29 00:43:53 +0900

    * refactor the front-server. ** divide the parse and execute code into each method.

--- server/rest/main/visualize_server.py
+++ server/rest/main/visualize_server.py
@@ -10,7 +10,15 @@ from rest_controller import *
 
 # from http://ja.pymotw.com/2/BaseHTTPServer/index.html
 class VisualizeServer(BaseHTTPRequestHandler):
+
+    CONTENTS_ROOT_PATH = '../../contents/'
+    FAVICON_PATH = CONTENTS_ROOT_PATH + 'favicon.ico'
     
+    KEY_RESPONSE_SC = 'sc'
+    KEY_RESPONSE_BODY = 'body'
+
+    DEFAULT_PORT = 8280
+
     def do_GET(self):
         parsed_path = urlparse.urlparse(self.path)
         message_parts = [
@@ -38,59 +46,87 @@ class VisualizeServer(BaseHTTPRequestHandler):
         print message
 
         # dispatch.
-        sc = 200
-        contents_root_path = '../../contents/'
-        if parsed_path.path.startswith('/contents/'):
-            # 指定された名前のコンテンツを返す
-            contents_name = parsed_path.path.split('/contents/')[1]
-
-            # ルートを指定された場合はindex.htmlを返すように設定
-            if len(contents_name) == 0:
-                contents_name = 'index.html'
-                    
-            # 指定された名前のコンテンツが上位ディレクトリを見ていないかチェック
-            abs_contents_root = os.path.abspath(contents_root_path)
-            abs_user_path = os.path.abspath(contents_root_path + contents_name)
-
-            if not abs_user_path.startswith(abs_contents_root):
-                # 上位ディレクトリを見ようとしているためNG
-                sc = 400
-                message = 'Bad Request.\r\n' + message
-
-            elif os.path.exists(abs_user_path):
-                # 上位ディレクトリを見ようとしておらず、かつ指定ファイルが存在
-                message = self.load_file(contents_root_path + contents_name)
-            else:
-                # 上位ディレクトリを見ていないが、指定ファイルがない
-                sc = 404
-                message = 'File is not found.\r\n' + message
-                
-        elif parsed_path.path.startswith('/rest'):
-            # execute.
-            controller = RestController()
-            response = controller.execute_with_subcontroller(
-                parsed_path.path.split('rest/')[1],
-                parsed_path.query)
-            print response
-        elif parsed_path.path.startswith('/favicon.ico'):
-            # contentsあたりにfaviconがあれば返す
-            if os.path.exists(contents_root_path + 'favicon.ico'):
-                message = self.load_file(contents_root_path + 'favicon.ico')
-            else: 
-                sc = 404
-                message = 'File is not found.\r\n' + message
-        else:
-            # 許可していないリクエストはとりあえず404にしておく
-            # RFC的にもOKっぽいので
-            sc = 404
-
+        # パフォーマンスを気にする場合は、self.wfileを渡して、ストリーム処理させるべき？
+        response = (self.dispatch_request(parsed_path.path))(
+            parsed_path.path, parsed_path.query)
+        
         # construct response.
-        self.send_response(sc)
+        self.send_response(response[VisualizeServer.KEY_RESPONSE_SC])
         self.end_headers()
-        self.wfile.write(message)
+        self.wfile.write(response[VisualizeServer.KEY_RESPONSE_BODY])
         
         return
 
+    # リクエストされたパスに応じて必要な関数を返す
+    def dispatch_request(self, requested_path):
+        if requested_path.startswith('/contents/'):
+            return self.return_contents
+        elif requested_path.startswith('/rest'):
+            return self.execute_rest_request
+        elif requested_path.startswith('/favicon.ico'):
+            return self.return_favicon
+        else:
+            return self.return_not_found
+
+    # 指定パスのコンテンツを返す
+    def return_contents(self, requested_path, requested_query):
+        # 指定された名前のコンテンツを返す
+        contents_name = requested_path.split('/contents/')[1]
+        
+        # ルートを指定された場合はindex.htmlを返すように設定
+        if len(contents_name) == 0:
+            contents_name = 'index.html'
+        
+        # 指定された名前のコンテンツが上位ディレクトリを見ていないかチェック
+        abs_contents_root = os.path.abspath(
+            VisualizeServer.CONTENTS_ROOT_PATH)
+        abs_user_path = os.path.abspath(
+            VisualizeServer.CONTENTS_ROOT_PATH + contents_name)
+
+        response = VisualizeServer.create_empty_response()
+        if not abs_user_path.startswith(abs_contents_root):
+            # 上位ディレクトリを見ようとしているためNG
+            response[VisualizeServer.KEY_RESPONSE_SC] = 400
+            response[VisualizeServer.KEY_RESPONSE_BODY] = 'Bad Request.'
+
+        elif os.path.exists(abs_user_path):
+            # 上位ディレクトリを見ようとしておらず、かつ指定ファイルが存在
+            response[VisualizeServer.KEY_RESPONSE_BODY] = self.load_file(
+                abs_user_path)
+        else:
+            # 上位ディレクトリを見ていないが、指定ファイルがない
+            response = self.return_not_found(requested_path, requested_query)
+            
+        return response
+
+    # RESTリクエストを処理する
+    # 具体的な処理は専用コントローラに委譲
+    def execute_rest_request(self, requested_path, requested_query):
+        # execute.
+        controller = RestController()
+        response = controller.execute_with_subcontroller(
+            requested_path.split('rest/')[1], requested_query)
+        return response
+
+    # faviconがあれば返す
+    def return_favicon(self, requested_path, requested_query):
+        response = VisualizeServer.create_empty_response()
+        if os.path.exists(VisualizeServer.FAVICON_PATH):
+            # @todo キャッシュしておく？
+            response[VisualizeServer.KEY_RESPONSE_BODY] = self.load_file(
+                VisualizeServer.FAVICON_PATH)
+        else: 
+            response = self.return_not_found(
+                requested_path, requested_query)
+
+        return response
+
+    # 不許可リクエストなどには404を返す
+    def return_not_found(self, requested_path, requested_query):
+        return {VisualizeServer.KEY_RESPONSE_SC: 404,
+                VisualizeServer.KEY_RESPONSE_BODY: ('%s is not found.'
+                                                    % requested_path)}
+        
     # 指定パスのファイルからテキストで読み込んでくる
     def load_file(self, abs_file_path):
         message = ''
@@ -98,9 +134,15 @@ class VisualizeServer(BaseHTTPRequestHandler):
             message += line
         return message
         
+    @staticmethod
+    def create_empty_response():
+        return {VisualizeServer.KEY_RESPONSE_SC: 200,
+                VisualizeServer.KEY_RESPONSE_BODY: ''}
 
+    
 if __name__ == '__main__':
     from BaseHTTPServer import HTTPServer
-    server = HTTPServer(('localhost', 8280), VisualizeServer)
+    server = HTTPServer(('localhost', VisualizeServer.DEFAULT_PORT),
+                        VisualizeServer)
     print 'Starting server, use <Ctrl-C> to stop'
     server.serve_forever()
